92fa93dcc91b9e8b2302ac8006dd5a3dc98df709
Alex-maslo <4439897@gmail.com>
2025-07-10 16:25:03 +0300

    fetch: users from API and render list in Next.js page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,19 @@
+import React from "react";
 import Navbar from "@/components/Navbar";
 
-export default function Home() {
-  return <Navbar />;
-}
+const Page = async () => {
+  const data = await fetch("https://backend-exam-kk3i.onrender.com/users");
+  const users = await data.json();
+  console.log(users);
+
+  return (
+    <div>
+      <Navbar />
+      <div className="w-full flex justify-center">
+        <h2>Hello amigo</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
