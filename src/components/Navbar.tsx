48e2f5c6484416e80930f53069e5359646b0806a
Alex-maslo <4439897@gmail.com>
2025-07-10 16:08:13 +0300

    feat: deploy frontend with Next.js setup for Vercel

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="flex justify-center  navbar bg-primary text-primary-content">
+        <button className="btn btn-ghost text-xl">daisyUI</button>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
